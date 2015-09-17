e1357bea7b3a89b54a16672275dbcafa05afdad8
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-17 16:20:07 +0300

    Fix paths

--- gulp/make.js
+++ gulp/make.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var path = require('path');
 var header = require('gulp-header');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
@@ -9,18 +10,18 @@ var pkg = require('../package.json');
 var banner = ['/**',
   ' * Ex - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.repository.url %>',
+  ' * @link <%= pkg.homepage %>',
   ' * @license <%= pkg.license %>',
   ' */',
   ''].join('\n');
 
 function make(browser, dir) {
-  return gulp.src('./src/ex.js')
-    .pipe( tributary( gulp.src('./src/*/' + browser + '.js').pipe(concat('scripts')) ) )
+  return gulp.src(path.join(__dirname, '../src/ex.js'))
+    .pipe( tributary( gulp.src(path.join(__dirname, '../src/*/' + browser + '.js') ).pipe(concat('scripts')) ) )
     .pipe(eslint()).pipe(eslint.format()).pipe(eslint.failOnError())
     .pipe(uglify())
     .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest((dir||'./dist/') + browser + '/'));
+    .pipe( gulp.dest(path.join((dir||'./dist/'), browser + '/')) );
 }
 
 module.exports = make;
