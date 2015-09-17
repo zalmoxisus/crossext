a2909b588f48bc3088f3dc5f6bcfcfa9363a6018
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-17 13:15:36 +0300

    Check the bundle with eslint

--- gulp/make.js
+++ gulp/make.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var header = require('gulp-header');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var eslint = require('gulp-eslint');
 var tributary = require('gulp-tributary');
 
 var pkg = require('../package.json');
@@ -16,6 +17,7 @@ var banner = ['/**',
 function make(browser, dir) {
   return gulp.src('./src/ex.js')
     .pipe( tributary( gulp.src('./src/*/' + browser + '.js').pipe(concat('scripts')) ) )
+    .pipe(eslint()).pipe(eslint.format()).pipe(eslint.failOnError())
     .pipe(uglify())
     .pipe(header(banner, {pkg: pkg}))
     .pipe(gulp.dest((dir||'./dist/') + browser + '/'));
